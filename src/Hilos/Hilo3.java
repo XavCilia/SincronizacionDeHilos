60fc052eee85db4b413b54fa751e78d264574f1c
Xavier <99370308+XavCilia@users.noreply.github.com>
2022-03-09 22:20:24 -0600

    Add files via upload
    2da Actualización

---
+++ src/Hilos/Hilo3.java
@@ -0,0 +1,19 @@
+package Hilos;
+
+public class Hilo3 extends Thread {
+        
+    @Override
+    public void run() {
+        for (int i = 0; i <= 5; i++) {
+            System.out.print("E");
+                        
+            try {
+                           Hilo3.sleep(1000); 
+            } catch (InterruptedException e) {
+                System.out.println("Error en el Hilo3" + e);
+            }
+      
+        }
+    }
+    
+}
---
+++ src/Hilos/Hilo4.java
@@ -0,0 +1,19 @@
+package Hilos;
+
+public class Hilo4 extends Thread {
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("K");
+                        
+            try {
+                           Hilo4.sleep(1000); 
+            } catch (InterruptedException e) {
+                System.out.println("Error en el Hilo4" + e);
+            }
+      
+        }
+    }
+
+}
